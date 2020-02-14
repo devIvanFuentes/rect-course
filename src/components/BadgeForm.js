4424600626ae586089b7dd225e2aa846d585b75f
Ivan Fuentes <ivanfuentesglez@gmail.com>
2020-02-14 14:25:31 -0600

    Inicio de creacion formulario

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class BadgeForm extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>New attendant</h1>
+                <form action="">
+                    <div className="form-group">
+                        <label>First name</label>
+                    </div>
+                </form>
+            </div>
+        )
+        
+    }
+}
+
+export default BadgeForm
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,6 +4,7 @@ import './styles/BadgeNew.css'
 import Navbar from '../components/Navbar'
 import header from '../images/badge-header.svg'
 import Badge from '../components/Badge'
+import BadgeForm from '../components/BadgeForm'
 
 class BadgeNew extends React.Component{
     render(){
@@ -17,7 +18,7 @@ class BadgeNew extends React.Component{
 
                 <div className="container">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-6">
                             <Badge 
                                 firstName="Iván" 
                                 lastName="Fuentes" 
@@ -25,6 +26,10 @@ class BadgeNew extends React.Component{
                                 twitter="devfuentes"
                                 avatarUrl="https://scontent.fmex6-1.fna.fbcdn.net/v/t1.0-9/67933988_2520461734680345_8614090312706424832_n.jpg?_nc_cat=105&_nc_ohc=S9W40Ag4AMsAX84bwKj&_nc_ht=scontent.fmex6-1.fna&oh=7b9e0395751ab7962ec5288483dc46db&oe=5F00F15C"
                             />
+                        </div>
+
+                        <div className="col-6">
+                            <BadgeForm />
                         </div>
                     </div>
                 </div>
